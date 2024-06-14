0328b2ae6607dbbd01e6f590b815f0054c4987f5
Raphael Cavalcante <133365398+RaphaelsPereira@users.noreply.github.com>
2024-06-14 08:53:56 -0300

    Add files via upload

---
+++ Csharp 5/program.cs
@@ -0,0 +1,73 @@
+using System;
+namespace QuintaAtividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*Primeiro Exercício: Tabuada*/
+
+
+            /*
+            Console.WriteLine("\nInsira um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine(numero + "x" + i + " = " + resultado);
+            }
+            */
+
+
+
+            /*Segundo Exercício: Pular 3 em 3*/
+
+
+            /*
+            Console.WriteLine("\nInsira um valor limite:");
+            int valorLimiteDe3Em3 = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= valorLimiteDe3Em3; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+
+
+            /*Terceiro Exercício: Especial*/
+
+            /*
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("\nInsira um valor:");
+                int valorEspecial = int.Parse(Console.ReadLine());
+
+                if (valorEspecial >= 100)
+                {
+                    Console.WriteLine("Número Especial");
+                }
+                else
+                {
+                    Console.WriteLine("Número Comum");
+                }
+            }
+            */
+
+
+
+            /*Quarto Exercício: Repetir*/
+
+            /*
+            Console.WriteLine("Deseja Iniciar?");
+            char resposta = char.Parse(Console.ReadLine());
+            while (resposta == 's')
+            {
+                Console.WriteLine("Deseja Continuar?");
+                resposta = char.Parse(Console.ReadLine());
+            }
+            */
+        }
+    }
+}
